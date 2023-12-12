0633a0b2293a62b2e4d65deb25d42dfa761e8110
Hugo Arcanjo <hfarcanjo@gmail.com>
2023-12-12 12:18:45 -0300

    feat: switch toggle image script

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 function toggleMode() {
   const html = document.documentElement
-
-  // if (body.classList.contains("light")) {
-  //   html.classList.remove("light")
-  // } else {
-  //   html.classList.add("light")
-  // }
-
   html.classList.toggle("light")
+
+  const img = document.querySelector("#profile img")
+  if (html.classList.contains("light")) {
+    img.setAttribute("src", "./assets/avatar-light.png")
+    img.setAttribute(
+      "alt",
+      "Foto de Mayk Brito sorrindo, usando óculos escuros e jaqueta preta, barba e fundo degrade roxo"
+    )
+  } else {
+    img.setAttribute("src", "./assets/avatar.png")
+    img.setAttribute(
+      "alt",
+      "Foto de Mayk Brito sorrindo, usando óculos e camisa preta, barba e fundo amarelo"
+    )
+  }
 }
